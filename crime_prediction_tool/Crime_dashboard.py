16e90bf6fc0a59222b0cc10b63e65d9050ffe8d7
Joseph Bradley <hermite2010@gmail.com>
2024-03-22 11:54:02 -0600

    started Dashboard

---
+++ crime_prediction_tool/Crime_dashboard.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import plotly.express as px
+import seaborn as sns
+
+st.title('Chicago District 11 Crime Prediction Tool')
